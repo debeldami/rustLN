9e1a5615df8b75646cba9ab34dce89f125688550
debeldami <debeldamilola@gmail.com>
2022-08-25 18:46:48 +0100

    a18

---
+++ src/bin/a18.rs
@@ -0,0 +1,32 @@
+// Topic: Result
+//
+// Requirements:
+// * Determine if a customer is able to make a restricted purchase
+// * Restricted purchases require that the age of the customer
+//   is at least 21
+//
+// Notes:
+// * Use a struct to store at least the age of a customer
+// * Use a function to determine if a customer can make a restricted purchase
+// * Return a result from the function
+// * The Err variant should detail the reason why they cannot make a purchase
+
+struct  Customer {
+    age: i32,
+}
+
+fn rest_purchase(customer: Customer) -> Result<(), String> {
+    match customer.age > 21 {
+        true => Ok(()),
+        false => Err("grow up".to_owned())
+    }
+}
+
+fn main() {
+    let customer = Customer{
+        age: 13
+    };
+    let is_restricted = rest_purchase(customer);
+
+    println!("{:?}", is_restricted)
+}
