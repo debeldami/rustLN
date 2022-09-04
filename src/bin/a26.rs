257669ee40eedcd345bbbee73d111c2388007059
debeldami <debeldamilola@gmail.com>
2022-09-04 19:15:11 +0100

    a26

---
+++ src/bin/a26.rs
@@ -0,0 +1,17 @@
+// Topic: External crates
+//
+// Requirements:
+// * Display the current date and time
+//
+// Notes:
+// * Use the `chrono` crate to work with time
+// * (OPTIONAL) Read the documentation section `Formatting and Parsing`
+//   for examples on how to create custom time formats
+use chrono::prelude::*;
+
+fn main() {
+    let utc: DateTime<Utc> = Utc::now();
+    // let local: DateTime<Local> = Local::now();
+
+    println!("{}", utc.format("%H:%M:%S %Y-%m-%d").to_string());
+}
