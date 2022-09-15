c7a6f385854f284d7f6cf649d63c1bf1eae87cf7
debeldami <debeldamilola@gmail.com>
2022-09-15 15:06:52 +0100

    a31

---
+++ src/bin/a31.rs
@@ -0,0 +1,61 @@
+// Topic: Trait Objects
+//
+// Summary:
+//   A contractor wants a program that can sum the cost of materials based
+//   on how many square meters are required for a job.
+//
+// Requirements:
+// * Calculate multiple material types with different costs
+// * Must be able to process a list of varying materials
+// * Material types and cost includes:
+//   * Carpet - $10 per square meter
+//   * Tile - $15 per square meter
+//   * Wood - $20 per square meter
+// * Square meters must be taken into account
+//
+// Notes:
+// * Create a trait that can be used to retrieve the cost of a material
+// * Create trait objects and store them in a vector for processing
+// * Use a function to calculate the total cost
+// * Process at least 3 different materials
+
+trait Material {
+    fn cost(&self) -> f64;
+}
+
+struct Carpet(f64);
+impl Material for Carpet {
+    fn cost(&self) -> f64 {
+        self.0 * 10.0
+    }
+}
+
+struct Tile(f64);
+impl Material for Tile {
+    fn cost(&self) -> f64 {
+        self.0 * 15.0
+    }
+}
+
+struct Wood(f64);
+impl Material for Wood {
+    fn cost(&self) -> f64 {
+        self.0 * 20.0
+    }
+}
+
+fn total_cost(items: Vec<Box<dyn Material>>) -> f64 {
+    items.iter().map(|item| item.cost()).sum()
+}
+
+fn main() {
+    let carpet = Box::new(Carpet(3.0));
+    let tile = Box::new(Tile(2.0));
+    let wood = Box::new(Wood(1.0));
+
+    let items: Vec<Box<dyn Material>> = vec![carpet, tile, wood];
+
+    let total_cost = total_cost(items);
+
+    println!("{}", total_cost);
+}
