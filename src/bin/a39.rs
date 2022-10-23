699217c691b00b7034b8631f5f3eb91463b5450f
debeldami <debeldamilola@gmail.com>
2022-10-23 17:30:40 +0100

    a39b

--- src/bin/a39.rs
+++ src/bin/a39.rs
@@ -1,116 +1,86 @@
-// Topic: TryFrom/TryInto
+// Topic: Channels
 //
 // Summary:
-// * A library is needed for an application to convert hex color codes
-//   into their component color values (red, green, and blue). Hex color codes
-//   consist of a hash symbol followed by six hex digits. Every two hex digits
-//   represent a color component in the order of red, green, blue.
-//
-//   Example hex color codes:
-//    #ffffff -> Rgb(255, 255, 255)
-//    #001122 -> Rgb(0, 17, 34)
+//   Using the existing code, create a program that simulates an internet-of-things
+//   remote control light bulb. The color of the light can be changed remotely.
+//   Use threads and channels to communicate what color the light bulb should display.
 //
 // Requirements:
-// * Create a program to convert a hex code (as &str) into an Rgb struct
-// * Implement TryFrom to perform the conversion
-// * Utilize the question mark operator in your implementation
+// * Create a separate thread representing the light bulb
+// * Use a channel to communicate with the thread
+// * Display a color change message using the println! macro
+// * The light bulb must also be able to turn on and off
+//   * Display whether the light is on or off on each color change
+// * Turn off the light when disconnecting from it
 //
 // Notes:
-// * See the `from_str_radix` function in the stdlib docs for `u8`
-//   to convert hex digits to `u8`
-//   * Hex digits use a radix value of 16
-// * Utilize the `thiserror` crate for your error type
-// * Run `cargo test --bin a37` to test your implementation
+// * Remember to add `crossbeam-channel` to your Cargo.toml file
+// * Use the `colored` crate if you want to get fancy and display actual colors
+// * The docs.rs site can be used to read documentation for third-party crates
+// * Disconnection can be accomplished by dropping the sender, or
+//   by telling the thread to self-terminate
+// * Use `cargo test --bin a39` to test your program to ensure all cases are covered
 
-use std::convert::TryFrom;
-use thiserror::Error;
+use crossbeam_channel::{unbounded, Receiver};
+use std::thread::{self, JoinHandle};
 
-#[derive(Debug, Error)]
-enum RgbError {
-    #[error("hex colors must begin with a hash (#)")]
-    MissingHash,
-    #[error("failed to parse hex digit: {0}")]
-    ParseError(std::num::ParseIntError),
-    #[error("wrong number of hex digits")]
-    ColorComponentError,
+enum LightMsg {
+    // Add additional variants needed to complete the exercise
+    ChangeColor(u8, u8, u8),
+    Disconnect,
 }
 
-#[derive(Debug, Eq, PartialEq)]
-struct Rgb(u8, u8, u8);
-
-impl TryFrom<&str> for Rgb {
-    type Error = RgbError;
-
-    fn try_from(hex: &str) -> Result<Self, Self::Error> {
-        if !hex.starts_with('#') {
-            return Err(RgbError::MissingHash);
-        }
-        if hex.len() != 7 {
-            return Err(RgbError::ColorComponentError);
-        }
-
-        let (r, g, b) = (
-            u8::from_str_radix(&hex[1..=2], 16)?,
-            u8::from_str_radix(&hex[3..=4], 16)?,
-            u8::from_str_radix(&hex[5..=6], 16)?,
-        );
-
-        Ok(Self(r, g, b))
-    }
+enum LightStatus {
+    Off,
+    On,
 }
 
-impl From<std::num::ParseIntError> for RgbError {
-    fn from(err: std::num::ParseIntError) -> Self {
-        Self::ParseError(err)
-    }
+fn spawn_light_thread(receiver: Receiver<LightMsg>) -> JoinHandle<LightStatus> {
+    // Add code here to spawn a thread to control the light bulb
+    let result = thread::spawn(move || match receiver.recv() {
+        Ok(lightMsg) => match lightMsg {
+            LightMsg::ChangeColor(_, _, _) => LightStatus::On,
+
+            LightMsg::Disconnect => LightStatus::Off,
+        },
+        Err(_) => LightStatus::Off,
+    });
+
+    result
 }
 
-fn main() {
-    // Use `cargo test --bin a37` to test your implementation
-}
+fn main() {}
 
 #[cfg(test)]
 mod test {
-    use super::Rgb;
-    use std::convert::TryFrom;
+    use super::*;
+    use crossbeam_channel::unbounded;
 
     #[test]
-    fn converts_valid_hex_color() {
-        let expected = Rgb(0, 204, 102);
-        let actual = Rgb::try_from("#00cc66");
-        assert!(actual.is_ok(), "valid hex code should be converted to Rgb");
-        assert_eq!(actual.unwrap(), expected, "wrong Rgb value");
+    fn light_off_when_disconnect() {
+        let (s, r) = unbounded();
+
+        let light = spawn_light_thread(r);
+        s.send(LightMsg::Disconnect).expect("channel disconnected");
+
+        let light_status = light.join().expect("failed to join light thread");
+
+        if let LightStatus::On = light_status {
+            panic!("light should be off after disconnection");
+        }
     }
 
     #[test]
-    fn fails_on_invalid_hex_digits() {
-        assert!(
-            Rgb::try_from("#0011yy").is_err(),
-            "should be an error with invalid hex color"
-        );
-    }
+    fn light_off_when_dropped() {
+        let (s, r) = unbounded();
 
-    #[test]
-    fn fails_when_missing_hash() {
-        assert!(
-            Rgb::try_from("001100").is_err(),
-            "should be an error when missing hash symbol"
-        );
-    }
+        let light = spawn_light_thread(r);
+        drop(s);
 
-    #[test]
-    fn fails_when_missing_color_components() {
-        assert!(
-            Rgb::try_from("#0011f").is_err(),
-            "should be an error when missing one or more color components"
-        );
-    }
+        let light_status = light.join().expect("failed to join light thread");
 
-    #[test]
-    fn fails_with_too_many_color_components() {
-        assert!(
-            Rgb::try_from("#0011ffa").is_err(),
-            "should be an error when too many color components are provided"
-        );
+        if let LightStatus::On = light_status {
+            panic!("light should be off after dropping sender");
+        }
     }
 }
